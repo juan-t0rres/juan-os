b9a72e10e6974018d0b2be15f9fced5c95b23372
Juan Torres <juanditb@gmail.com>
2022-11-22 19:38:39 -0500

    Bug fixes

--- src/windows/Window.js
+++ src/windows/Window.js
@@ -1,30 +1,41 @@
 import { useState } from "react";
 import Draggable from "react-draggable";
 
-export default function Window({ title, children, removeWindow, onDrag, window, zIndex }) {
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+export default function Window({
+  title,
+  children,
+  removeWindow,
+  onDrag,
+  window,
+  zIndex,
+}) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-    function handleDrag(e, ui) {
-        const { x, y } = position;
-        onDrag(window);
-        setPosition({
-            x: x + ui.deltaX,
-            y: y + ui.deltaY,
-        });
-    }
+  function handleDrag(e, ui) {
+    const { x, y } = position;
+    onDrag(window);
+    setPosition({
+      x: x + ui.deltaX,
+      y: y + ui.deltaY,
+    });
+  }
 
-    return (
-        <Draggable onDrag={handleDrag} position={position}>
-            <div className="window" style={{ zIndex: zIndex }}>
-                <div className="bar pixel-font">
-                    <div />
-                    <div>{title}</div>
-                    <div onClick={removeWindow} className="remove-window">
-                        X
-                    </div>
-                </div>
-                <div className="window-content">{children}</div>
-            </div>
-        </Draggable>
-    );
+  return (
+    <Draggable
+      onDrag={handleDrag}
+      position={position}
+      positionOffset={{ x: "-50%", y: "-50%" }}
+    >
+      <div className="window" style={{ zIndex: zIndex }}>
+        <div className="bar pixel-font">
+          <div />
+          <div>{title}</div>
+          <div onClick={removeWindow} className="remove-window">
+            X
+          </div>
+        </div>
+        <div className="window-content">{children}</div>
+      </div>
+    </Draggable>
+  );
 }
